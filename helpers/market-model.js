96558ec0ffafdccf488a9d26628479522ed943ff
taterntots <matthew.a.weidner@gmail.com>
2020-03-27 21:26:26 -0400

    got adding turnip prices to work, but returns errors

--- helpers/market-model.js
+++ helpers/market-model.js
@@ -1,18 +1,50 @@
 const db = require('../data/dbConfig');
 
 module.exports = {
-  findTurnips
+  findTurnips,
+  findTurnipPriceById,
+  addTurnipPrice
 };
 
 // FIND ALL TURNIPS
 function findTurnips() {
   return db('turnips as t')
-  .select(
-    't.id as turnip_id',
-    'v.villager_name',
-    'v.island_name',
-    't.morning_price',
-    't.afternoon_price'
-  )
+    .select(
+      't.id as turnip_id',
+      'v.villager_name',
+      'v.island_name',
+      't.morning_price',
+      't.afternoon_price'
+    )
     .join('villagers as v', 'v.id', 't.villager_id');
+}
+
+// FIND TURNIP PRICE BY ID
+// function findTurnipPriceById(turnId) {
+//   return db('turnips as t')
+//   .select(
+//     't.id as turnip_id',
+//     'v.villager_name',
+//     'v.island_name',
+//     't.morning_price',
+//     't.afternoon_price'
+//   )
+//     .join('villagers as v', 'v.id', 't.villager_id')
+//     .where('t.id', turnId)
+//     .first();
+// }
+
+function findTurnipPriceById(id) {
+  return db('turnips')
+    .where({ id }, turnId)
+    .first();
+}
+
+// ADD A TURNIP PRICE
+function addTurnipPrice(newTurnipPrice) {
+  return db('turnips')
+    .insert(newTurnipPrice, 'id')
+    .then(([id]) => {
+      return findTurnipPriceById(id);
+    });
 }
--- routers/market-router.js
+++ routers/market-router.js
@@ -1,9 +1,9 @@
 const router = require('express').Router();
-const Revs = require('../helpers/market-model.js');
+const Mar = require('../helpers/market-model.js');
 
 //************* GET ALL TURNIPS ***************//
 router.get('/', (req, res) => {
-  Revs.findTurnips()
+  Mar.findTurnips()
     .then(turnips => {
       res.json(turnips);
     })
@@ -13,5 +13,22 @@ router.get('/', (req, res) => {
       });
     });
 });
+
+//***************** ADD NEW REVIEW *******************//
+router.post(
+  '/:villagerId/add-turnip-price', (req, res) => {
+    const { villagerId } = req.params;
+    let prices = req.body;
+    prices = { ...prices, villager_id: villagerId };
+
+    Mar.addTurnipPrice(prices)
+      .then(newTurnipPrice => {
+        res.status(201).json(newTurnipPrice);
+      })
+      .catch(err => {
+        res.status(500).json({ error: 'There was an error adding the turnip price' });
+      });
+  }
+);
 
 module.exports = router;
