f2ec660fc85cf981a52f5d3dbd57ab3b7ffc6547
taterntots <matthew.a.weidner@gmail.com>
2020-03-30 17:32:21 -0400

    added some more seed data for testing get requests

--- data/seeds/01-villagers.js
+++ data/seeds/01-villagers.js
@@ -6,7 +6,9 @@ exports.seed = function (knex) {
       // Inserts seed entries
       return knex('villagers').insert([
         {
-          villager_name: 'Tater', island_name: 'Memoria', password: 'farts'
+          villager_name: 'Tater', island_name: 'Memoria', password: 'Tater',
+          villager_name: 'Tots', island_name: 'BaddaBoo', password: 'Tots',
+          villager_name: 'Pugsley', island_name: 'PugTown', password: 'Pugsley'
         }
       ]);
     });
