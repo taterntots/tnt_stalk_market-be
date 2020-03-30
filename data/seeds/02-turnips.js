e771a2a3ca7d06b88fd11d52444b4570bff7f023
taterntots <matthew.a.weidner@gmail.com>
2020-03-30 18:01:01 -0400

    added turnip_id to seeds

--- data/seeds/02-turnips.js
+++ data/seeds/02-turnips.js
@@ -6,9 +6,9 @@ exports.seed = function (knex) {
       // Inserts seed entries
       return knex('turnips').insert([
         {
-          villager_id: 1, morning_price: 56, afternoon_price: 145,
-          villager_id: 2, morning_price: 98, afternoon_price: 23,
-          villager_id: 3, morning_price: 195, afternoon_price: 64
+          turnip_id: 1, villager_id: 1, morning_price: 56, afternoon_price: 145,
+          turnip_id: 2, villager_id: 2, morning_price: 98, afternoon_price: 23,
+          turnip_id: 3, villager_id: 3, morning_price: 195, afternoon_price: 64
         }
       ]);
     });
