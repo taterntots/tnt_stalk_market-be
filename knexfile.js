01434645edd20ff5dcddb6eeab24604ab1a6fd93
Matthew Weidner <matthew.a.weidner@gmail.com>
2021-08-19 18:48:14 -0500

    created a gitignore and removed nodemodules folder

--- knexfile.js
+++ knexfile.js
@@ -3,27 +3,6 @@ require('dotenv').config();
 
 module.exports = {
 
-  // development: {
-  //   client: 'sqlite3',
-  //   connection: {
-  //     filename: './data/catalogue.db3'
-  //   },
-  //   useNullAsDefault: true,
-  //   migrations: {
-  //     directory: './data/migrations'
-  //   },
-  //   seeds: {
-  //     directory: './data/seeds'
-  //   },
-  //   // SQLite will not enforce foreign key constraints by default
-  //   // ONLY NEEDED FOR SQLITE
-  //   pool: {
-  //     afterCreate: (conn, done) => {
-  //       conn.run("PRAGMA foreign_keys = ON", done); // turn on foreign key enforcement
-  //     }
-  //   }
-  // },
-
   development: {
     client: 'pg',
     connection: process.env.DB_DEV,
@@ -45,5 +24,4 @@ module.exports = {
       directory: './data/seeds'
     }
   }
-
 };
